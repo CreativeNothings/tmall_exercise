75612366606f0593db471342da81f2a6ab9f743a
createNothings <1065164542@qq.com>
2024-03-26 15:14:19 +0800

    属性管理已完成

---
+++ src/main/java/com/how2java/tmall/dao/PropertyDAO.java
@@ -0,0 +1,15 @@
+package com.how2java.tmall.dao;
+
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.Property;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+
+public interface PropertyDAO extends JpaRepository<Property,Integer> {
+Page<Property> findByCategory(Category category, Pageable pageable);
+}
